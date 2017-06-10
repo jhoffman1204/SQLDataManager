fae4c319d3f0914556d2f8570ab8489d37b198fd
james.hoffman <james.a.hoffman@stonybrook.edu>
2017-06-09 23:02:41 -0400

    test commit for brian branch

--- src/GUI/ClassGUI/ClassGUI.java
+++ src/GUI/ClassGUI/ClassGUI.java
@@ -2,6 +2,9 @@ package GUI.ClassGUI;
 
 /**
  * Created by James on 6/9/2017.
+ *
+ * This class will setup all panes and references to controllers that deal with creating, updating and viewing classes
  */
 public class ClassGUI {
+
 }
